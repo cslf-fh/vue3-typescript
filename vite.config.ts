b4a3f8834ade15d6d68485749959de2908b9e718
cslf-fh <consadole.sapporo.love.football@gmail.com>
2022-02-15 18:16:04 +0900

    add vuetify

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { fileURLToPath, URL } from 'url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vuetify from '@vuetify/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vuetify({
+      autoImport: true,
+    }),
+  ],
+  define: { 'process.env': {} },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
